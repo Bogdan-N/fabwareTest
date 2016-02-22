6c6547a5418e45adb5f3b3f15e8a11d4e4fe4739
BNosachenko <nosachenko@outlook.com>
2016-02-22 23:33:53 +0200

    Created template with yeomen

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fabwareTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fabwareTestApp
+ */
+angular.module('fabwareTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
