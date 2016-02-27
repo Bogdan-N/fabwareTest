5a1744aff23db2144861e14a9b7aff97f641b71a
BNosachenko <nosachenko@outlook.com>
2016-02-28 00:35:00 +0200

    finished second page and refactoring

---
+++ src/app/common/filmsInYear.service.js
@@ -0,0 +1,70 @@
+(function(){
+	angular.module('fabwareTest.common')
+	.factory('createTableService', [ function(){
+
+		
+
+		var filmsInYear = function(){
+
+			var sortYears = function(years) {
+			years.sort(function(a, b){
+				if (a > b) {
+					return 1;
+				} else{
+					return -1
+				}
+			})
+		};
+
+		var getFilmsInYear = function(years) {
+			var result = {},
+			 	yearsLabels = [],
+			 	countArray = [];
+
+			 sortYears(years);
+
+			for(var i = 0; i < years.length; ++i){
+				var count = 1;
+				yearsLabels.push(years[i]);
+
+				while(years[i] === years[i+1]){
+					++i
+					++count;
+				}
+				countArray.push(count);
+			}
+			result.years = yearsLabels;
+			result.count = countArray;
+			return result;
+		};
+
+			return {
+
+				initTable: function(years){
+
+					var ctx = document.getElementById("myChart").getContext("2d"),
+	            		filmsInYear = getFilmsInYear(years),
+	            		data = {
+					    labels: filmsInYear.years,
+					    datasets: [
+					        {
+					            fillColor: "rgba(151,187,205,0.5)",
+					            strokeColor: "rgba(151,187,205,0.8)",
+					            highlightFill: "rgba(151,187,205,0.75)",
+					            highlightStroke: "rgba(151,187,205,1)",
+					            data: filmsInYear.count
+					        }
+					    ]
+					};
+
+					
+					var myBarChart = new Chart(ctx).Bar(data);
+				}
+			}
+		}
+
+		return new filmsInYear();
+
+	}])
+
+})();
--- src/app/components/decade/decade.controller.js
+++ src/app/components/decade/decade.controller.js
@@ -1,14 +1,14 @@
 (function(){
 
 	angular.module('fabwareTest.decade')
-	.controller('decadeController', ['filmsListResource',/* 'quickSort', */function(filmsListResource/*, quickSort*/){
+	.controller('decadeController', ['filmsListResource', 'createTableService', function(filmsListResource, createTableService){
 		decadeController = Class.extend({
 			init: function(){
 
-				this._initTable();
+				this.getFilmsInfo();
 			},
 
-			/*getFilmsInfo: function(){
+			getFilmsInfo: function(){
 				filmsListResource.getList()
 				.then(function(response){
 					var response = response.data.data.movies;
@@ -16,35 +16,11 @@
 						return movie.year - (movie.year % 10);
 					});
 					
+					createTableService.initTable(years);
 
-
-				}.bind(this))
-			},
-
-			_sortYears: function(years){
-				var sortedYears = quickSort.sort(years);
-				return sortedYears;
-			}*/
-
-			_initTable: function(){
-				var ctx = document.getElementById("myChart").getContext("2d");
-            	
-            	var data = {
-				    labels: ["January", "February", "March", "April", "May", "June", "July"],
-				    datasets: [
-				        {
-				            fillColor: "rgba(151,187,205,0.5)",
-				            strokeColor: "rgba(151,187,205,0.8)",
-				            highlightFill: "rgba(151,187,205,0.75)",
-				            highlightStroke: "rgba(151,187,205,1)",
-				            data: [28, 48, 40, 19, 86, 27, 90]
-				        }
-				    ]
-				};
-
-				//getFilmsInfo();
-				var myBarChart = new Chart(ctx).Bar(data);
+				}.bind(this));
 			}
+			
 		})
 		return new decadeController();
 	}]);
